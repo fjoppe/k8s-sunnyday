0140c4cb335a92fb52a4e4ac2bcca2eae9f201b5
frankjoppe <frankjoppe@hotmail.com>
2024-01-16 17:16:12 +0000

    Setting up webserver

--- application/restapi/build.sh
+++ application/restapi/build.sh
@@ -1,1 +1,14 @@
 #!/bin/bash
+
+export workdir=$(pwd)
+export currentdir=`dirname -- "$( readlink -f -- "$0"; )";`
+ls $currentdir
+
+cd $currentdir/src
+
+npm install
+npm run build
+
+cd $currentdir
+
+docker build -t k8simage:restapi-latest .
